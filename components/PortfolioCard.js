050df15e1360ce86a424c1f3ae55faf5aa0c2c44
Christian Martinez <christian@mixdlabs.com>
2023-05-08 14:18:14 -0600

    Added back the overlay to the portfolio card image

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -13,35 +13,20 @@ const PortfolioCard = ({
 }) => {
   return (
     <div className='max-w-screen overflow-hidden rounded-lg shadow-lg'>
-      <Image
-        className='object-cover object-center h-48 w-full lg:w-96'
-        src={imageUrl}
-        alt={alt}
-        width={500}
-        height={500}
-      />
-      <div className='px-6 py-4 h-40'>
+      <div
+        className='bg-cover bg-center h-48 w-full lg:w-96'
+        style={{
+          backgroundImage: `url('${imageUrl}')`,
+        }}
+      >
+        <div class='w-full h-full flex justify-center items-center bg-gradient-to-r from-purple-600 to-pink-500 opacity-60'></div>
+      </div>
+      <div className='flex flex-col rounded-lg justify-center w-full h-full hover:shadow-2xl bg-gradient-to-r from-purple-600 to-pink-500 opacity-60 transition duration-300 ease-in-out hover:opacity-90'></div>
+      <div className='px-6 py-4 h-20'>
         <div className='font-bold text-xl mb-2'>{title}</div>
         <p className='text-gray-700 text-base'>{description}</p>
-        <div className='flex justify-start items-left my-5'>
-          <p className='text-gray-700 text-base my-2'>Project Links:</p>
-          <Link
-            href={repo}
-            className='text-gray-700 inline-flex items-center font-medium text-primary-600 hover:text-purple-600 transition duration-300 ease-in-out'
-          >
-            <FaGithub className='ml-3' />
-          </Link>
-
-          <Link
-            href={href}
-            className='text-gray-700 inline-flex items-center font-medium text-primary-600 hover:text-purple-600 transition duration-300 ease-in-out'
-          >
-            <FaLink className='ml-2' />
-          </Link>
-        </div>
       </div>
-
-      <div className='px-6 pt-4 pb-3'>
+      <div className='px-6 pt-4 pb-3 mt-10'>
         {tags.map((tag, i) => {
           return (
             <span
@@ -52,6 +37,22 @@ const PortfolioCard = ({
             </span>
           )
         })}
+      </div>
+      <div className='inline-flex px-6 pt-4 pb-3'>
+        <p className='text-gray-700 text-base my-2'>Project Links:</p>
+        <Link
+          href={repo}
+          className='text-gray-700 inline-flex items-center font-medium text-primary-600 hover:text-purple-600 transition duration-300 ease-in-out'
+        >
+          <FaGithub className='ml-5' />
+        </Link>
+
+        <Link
+          href={href}
+          className='text-gray-700 inline-flex items-center font-medium text-primary-600 hover:text-purple-600 transition duration-300 ease-in-out'
+        >
+          <FaLink className='ml-5' />
+        </Link>
       </div>
     </div>
   )
--- constants/index.js
+++ constants/index.js
@@ -14,7 +14,7 @@ const portfolioData = [
       'https://images.unsplash.com/photo-1611605698335-8b1569810432?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
     alt: 'Image for christians twitter bot project',
     description:
-      'An AI twitter bot that tweets every two hours about provided topics.',
+      'An AI twitter bot powered by ChatGPT that tweets every two hours about given topics.',
     repo: 'https://github.com/christianbmartinez/twitterbot',
     href: 'https://twitter.com/chaoticmuchbot',
     tags: ['#NODEJS', '#CHATGPT'],
@@ -24,7 +24,8 @@ const portfolioData = [
     imgUrl:
       'https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     alt: 'Image for christians colorfill app project',
-    description: 'A desktop tool for real time text fill animations.',
+    description:
+      'A desktop css animation configuration tool that displays real time text fill animations.',
     repo: 'https://github.com/christianbmartinez/colorfill',
     href: 'https://codepen.io/_coderchris/pen/KKgyywR',
     tags: ['#CSS VARIABLES', '#JS'],
@@ -34,7 +35,8 @@ const portfolioData = [
     imgUrl:
       'https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
     alt: 'Image for christians colorfilljs app project',
-    description: 'A text fill animation library made with javascript.',
+    description:
+      "A text fill animation library made with javascript and published to NPM's official website.",
     repo: 'https://github.com/christianbmartinez/colorfilljs',
     href: 'https://www.npmjs.com/package/colorfilljs',
     tags: ['#JS', '#NPM'],
@@ -45,7 +47,7 @@ const portfolioData = [
       'https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80',
     alt: 'Image for christians mmbc website project',
     description:
-      'A single page website for a business consulting and tax services company.',
+      "A single page application built with react and tailwind for MMBC's business consulting and tax services.",
     repo: 'https://github.com/christianbmartinez/mmbc',
     href: 'https://www.mmbc.llc/',
     tags: ['#TAILWIND', '#REACT'],
@@ -55,7 +57,8 @@ const portfolioData = [
     imgUrl:
       'https://images.unsplash.com/photo-1511690656952-34342bb7c2f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80',
     alt: 'Image for christians veegin project',
-    description: 'A full stack app for finding vegan only products.',
+    description:
+      'A full stack app for finding vegan only products using the Kroger API, Local Storage, and OAuth2',
     repo: 'https://github.com/christianbmartinez/veegin',
     href: '#',
     tags: ['#EXPRESS', '#MONGODB'],
@@ -65,7 +68,8 @@ const portfolioData = [
     imgUrl:
       'https://images.unsplash.com/photo-1615185990778-a15d7015b41d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80',
     alt: 'Image for christians mysteryboard project',
-    description: 'A full stack app for posting anonymous content.',
+    description:
+      'A full stack anonymous sharing application that uses mongo db and express for fetching and storing user data.',
     repo: 'https://github.com/christianbmartinez/mysteryboard',
     href: '#',
     tags: ['#EXPRESS', '#MONGODB'],
