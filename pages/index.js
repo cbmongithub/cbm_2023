91eff95c21c62e356faa2a7340234446b3e2155d
Christian Martinez <christian@mixdlabs.com>
2023-05-12 20:05:11 -0600

    Bumped up 20 in the score almost. Have to find an alternative to the particles on landing page. Possibly css animations

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import SiteHead from '../components/SiteHead'
 import Link from 'next/link'
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation'
+import Background from '../components/Background'
 import { motion } from 'framer-motion'
 
 const Index = () => {
@@ -90,6 +91,7 @@ const Index = () => {
             </div>
           </div>
         </div>
+        <Background />
       </section>
     </>
   )
