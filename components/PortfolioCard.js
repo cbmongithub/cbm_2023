2ef63af71ecbe255b174aa568a06fece931955dd
Christian Martinez <christian@mixdlabs.com>
2023-05-27 08:33:40 -0600

    Target _blank

--- components/PortfolioCard.js
+++ components/PortfolioCard.js
@@ -48,6 +48,7 @@ const PortfolioCard = ({
         </p>
         <Link
           href={repo}
+          target='_blank'
           aria-label='Christian B Martinez | Project Repository Link'
           className='text-zinc-800 dark:text-zinc-50 inline-flex items-center font-medium text-primary-600 hover:text-purple-600 dark:hover:text-purple-600 transition duration-300 ease-in-out'
         >
@@ -56,6 +57,7 @@ const PortfolioCard = ({
 
         <Link
           href={href}
+          target='_blank'
           aria-label='Christian B Martinez | Project View Live Link'
           className='text-zinc-800 dark:text-zinc-50 inline-flex items-center font-medium text-primary-600 hover:text-purple-600 dark:hover:text-purple-600 transition duration-300 ease-in-out'
         >
