70f6fc57ea009fc849e26e6f68e0b59bb7aa7024
Christian Martinez <christian@mixdlabs.com>
2023-09-24 13:10:12 -0600

    Netlify runtime errors

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    forceSwcTransforms: true,
-  },
   images: {
     domains: ['localhost', 'images.unsplash.com', 'giphy.com', 'gph.is'],
     remotePatterns: [
