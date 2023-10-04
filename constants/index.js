5e9a1e6d0256132a9c2fd025db74088f4138af47
Christian Martinez <christian@mixdlabs.com>
2023-10-03 20:59:23 -0600

    Moved svg path variants into constants file

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,103 @@
+const svgPathVariant1 = {
+  open: { d: 'M3.06061 2.99999L21.0606 21' },
+  closed: { d: 'M0 9.5L24 9.5' },
+}
+
+const svgPathVariant2 = {
+  open: { d: 'M3.00006 21.0607L21 3.06064' },
+  closed: { d: 'M0 14.5L15 14.5' },
+}
+
+const firstVariant = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 0.5,
+    },
+  },
+  closed: { y: -300, opacity: 0 },
+}
+const secondVariant = {
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 0.5,
+    },
+  },
+  closed: { x: -100, opacity: 0 },
+}
+
+const thirdVariant = {
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 0.75,
+    },
+  },
+  closed: { x: -100, opacity: 0 },
+}
+
+const fourthVariant = {
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 1,
+    },
+  },
+  closed: { x: -100, opacity: 0 },
+}
+
+const fifthVariant = {
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 1.25,
+    },
+  },
+  closed: { x: -100, opacity: 0 },
+}
+
+const sixthVariant = {
+  open: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 1.5,
+    },
+  },
+  closed: { x: -100, opacity: 0 },
+}
+
+const seventhVariant = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: 100,
+      delay: 1.75,
+    },
+  },
+  closed: { y: 300, opacity: 0 },
+}
+
 const socialLinks = {
   github: 'https://github.com/christianbmartinez',
   linkedin: 'https://www.linkedin.com/in/cmartinez1089/',
@@ -73,4 +173,16 @@ const portfolioData = [
   },
 ]
 
-export { socialLinks, portfolioData }
+export {
+  socialLinks,
+  portfolioData,
+  svgPathVariant1,
+  svgPathVariant2,
+  firstVariant,
+  secondVariant,
+  thirdVariant,
+  fourthVariant,
+  fifthVariant,
+  sixthVariant,
+  seventhVariant,
+}
