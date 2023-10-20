fb5d83b8a878f0fdfb94685188db41db1740cc47
Christian Martinez <christian@mixdlabs.com>
2023-10-20 17:45:37 -0600

    Added socials to the footer. Removed unused imports in socials. Added bg color for index page now the black bg should not flash. Changed z index and position for particles bg

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,9 @@
+import Socials from './Socials'
+
 const Footer = () => {
   return (
     <footer className='bg-zinc-50 py-4 pt-12 dark:bg-slate-950'>
+      <Socials style='flex flex-row justify-center items-center py-4' />
       <div className='relative z-0 mx-auto flex w-full items-center justify-center p-4'>
         <span className='text-md text-zinc-400 sm:text-center'>
           &copy;&nbsp;Christian B. Martinez {new Date().getFullYear()}
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { FaGithub, FaTiktok, FaCodepen, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { FaSquareXTwitter } from 'react-icons/fa6'
 
 import { socialLinks } from '../constants'
