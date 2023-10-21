616a878dcbae602c42374149f9aaa57c0f1b58aa
Christian Martinez <christian@mixdlabs.com>
2023-10-21 11:06:20 -0600

    Footer styling, layout changes, optimizing code

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,11 @@ import Socials from './Socials'
 
 const Footer = () => {
   return (
-    <footer className='bg-zinc-50 py-4 pt-12 dark:bg-slate-950'>
-      <Socials style='flex flex-row justify-center items-center py-4' />
-      <div className='relative z-0 mx-auto flex w-full items-center justify-center p-4'>
-        <span className='text-md text-zinc-400 sm:text-center'>
-          &copy;&nbsp;Christian B. Martinez {new Date().getFullYear()}
-        </span>
-      </div>
+    <footer className='relative bottom-3 z-40 w-full bg-transparent py-4'>
+      <Socials style='flex flex-row w-full justify-center items-center' />
+      <span className='md:text-md flex flex-row items-center justify-center pt-4 text-sm text-zinc-400 dark:text-zinc-500 '>
+        &copy;&nbsp;Christian B. Martinez {new Date().getFullYear()}
+      </span>
     </footer>
   )
 }
