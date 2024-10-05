38767881221c3668b37321afd1212abd60258c90
Christian Martinez <hello@christianbmartinez.com>
2024-10-05 06:06:26 -0600

    Will fix everything tomorrow. Still need to upgrade and configure everthing, and test.

--- pages/contact.js
+++ pages/contact.js
@@ -121,7 +121,7 @@ const Contact = () => {
 									</div>
 								</div>
 								<div className="mx-auto max-w-[700px]">
-									<form onSubmit={handleSubmit}>
+									<form onSubmit={handleSubmit} netlify>
 										<div className="form-group mb-6">
 											<input
 												className="form-control m-0
@@ -227,11 +227,7 @@ const Contact = () => {
 														xmlns="http://www.w3.org/2000/svg"
 														viewBox="0 0 20 20"
 														onClick={() => setFormData({ show: false })}
-														onKeyDown={(e) => {
-															if (e.key === "Enter" || e.key === " ") {
-																setFormData({ show: false });
-															}
-														}}
+														onKeyDown={handleSubmit}
 													>
 														<title>Close</title>
 														<path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
